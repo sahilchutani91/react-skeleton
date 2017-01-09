cc8255153424c756981c5c2098e3920f2a7674d7
sahil <sahil.chutani@prontoitlabs.com>
2017-01-09 11:20:08 +0530

    react skeleton

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react') ;
+var ReactDOM = require('react-dom');
+var List = require('./Components/List.jsx') ;
+
+ReactDOM.render(<List />, document.getElementById('ingredient')) ;
